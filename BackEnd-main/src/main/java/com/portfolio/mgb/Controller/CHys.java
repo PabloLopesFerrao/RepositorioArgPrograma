b259d3a76801276fcf15e3d13fe014900f5de553
Pablo <pablo_lopesferrao@hotmail.com>
2023-01-24 12:57:36 -0300

    agregando bases de datos

--- BackEnd-main/src/main/java/com/portfolio/mgb/Controller/CHys.java
+++ BackEnd-main/src/main/java/com/portfolio/mgb/Controller/CHys.java
@@ -20,10 +20,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/skill")
+
 //@CrossOrigin(origins = "http://localhost:4200")
 @CrossOrigin(origins = "https://pablolopesferraoargprograma.web.app")
-
+@RequestMapping("/skill")
 public class CHys {
 
     @Autowired
